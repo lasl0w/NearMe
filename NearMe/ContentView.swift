61d2ac2b9a535347cc3f88023b763fc0a14824ea
Tom Montgomery <montgomery3681@gmail.com>
2023-11-28 14:05:21 -0700

    Initial Commit

---
+++ NearMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearMe
+//
+//  Created by tom montgomery on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by tom montgomery on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
